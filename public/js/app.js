3679cea7f9978857c8461c982b8f47dbe1c56f2c
Ran <ranhoudine@gmail.com>
2023-10-12 13:01:28 +0300

    removed unnecessary console log at beginning of file

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,3 @@
-console.log('Client side javascript file is loaded')
-
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelectorAll('input')
 const messageOne = document.querySelector('#message-1')
@@ -28,4 +23,4 @@ weatherForm.addEventListener('submit', (e) => {
 
     })
 })
-})
+}) 
